128164ae9507b3dfba772234e0406e3082264973
noumedem tami florian <noumedemtamiflorian@gmail.com>
2023-02-03 11:45:20 +0100

    Ajoute de la variable DATABASE

--- src/config/db.js
+++ src/config/db.js
@@ -1,23 +1,23 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv")
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
 const DB_URI = process.env.DB_URI;
-const DATABASE="bmc-design"
+const DATABASE = process.env.DB_URI;
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(DB_URI, {
-      dbName : DATABASE,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    mongoose.set('strictQuery', false);
-    console.log("MongoDB Connected...");
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(DB_URI, {
+            dbName: DATABASE,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        mongoose.set("strictQuery", false);
+        console.log("MongoDB Connected...");
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
