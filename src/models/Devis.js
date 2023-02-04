d4090a843cb318be30b2b1d4297c1e77684115b6
noumedem tami florian <noumedemtamiflorian@gmail.com>
2023-02-04 16:21:12 +0100

    Modification du Schema pour le devis

--- src/models/Devis.js
+++ src/models/Devis.js
@@ -1,42 +1,33 @@
 const mongoose = require("mongoose");
 
 const DevisSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  need: {
-    type: String,
-    required: true
-  },
-  job: {
-    type: String,
-    required: true
-  },
-  startDate: {
-    type: Date,
-    required: true
-  },
-  budget: {
-    type: Number,
-    required: true
-  },
-  currency: {
-    type: String,
-    required: true
-  },
-  message: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    need: {
+        type: String,
+        required: true,
+    },
+    work_for_need: {
+        type: String,
+        required: true,
+    },
+    startDate: {
+        type: String,
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    message: {
+        type: String,
+    },
 });
 
 module.exports = mongoose.model("devis", DevisSchema);
